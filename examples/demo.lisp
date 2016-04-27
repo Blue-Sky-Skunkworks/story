3039e29dc83c7367d52f592c39425af14e06fa4e
conrad <whalliburton@gmail.com>
2016-04-27 11:08:15 -0600

    demos

--- examples/demo.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :story)
-
-
-(define-story demo-1 (:title "Story Demo 1")
-  (:h1 "Story Demo 1"))
---
+++ examples/demos.lisp
@@ -0,0 +1,6 @@
+(in-package :story)
+
+
+(define-story demo-a (:title "Story Demo A")
+  (:h1 "Story Demo A"))
+

57d4ca93d985aa0dc04283573b5ab31814f2a3bb
conrad <whalliburton@gmail.com>
2016-05-01 19:28:10 -0600

    packery

---
+++ modules/packery/packery.lisp
@@ -0,0 +1,16 @@
+(in-package :story)
+
+(define-story-module packery
+    :scripts ("packery.pkgd.min.js"
+              ("packery.js" packery)))
+
+(in-package :story-js)
+
+(define-script packery
+  (defun setup-packing (container-id item &optional (gutter 20))
+    (let ((container (get-by-id container-id)))
+      (if (@ container pack)
+          ((@ container pack layout))
+          (setf (@ container pack) (new (*packery container
+                                                  (create :item-selector (+ "." item)
+                                                          :gutter gutter))))))))

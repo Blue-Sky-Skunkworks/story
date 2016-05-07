c0d81b57f6535e2b7cc82c75beb3bd947c0f0a54
conrad <whalliburton@gmail.com>
2016-05-07 08:54:46 -0600

    toggle-production

--- configuration.lisp
+++ configuration.lisp
@@ -1,4 +1,7 @@
 (in-package :story)
 
-(defparameter *production* t)
+(defparameter *production* nil)
 
+(defun toggle-production ()
+  (setf *production* (not *production*))
+  (note ";; ~A in production." (if *production* "Now" "Not")))

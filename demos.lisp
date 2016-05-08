e762d5a153ed08c692c62e8b7ea7f4041af2ab3f
conrad <whalliburton@gmail.com>
2016-05-08 05:51:31 -0600

    save demo list for later

--- demos.lisp
+++ demos.lisp
@@ -1,10 +1,14 @@
 (in-package :story)
 
+(defvar *demos* nil)
+
 (defmacro define-demo (name modules &body body)
   (let ((title (format nil "Story Demo ~A" (string-capitalize name))))
-    `(define-story ,(symb 'demo- name) (:title ,title :modules ,modules)
-       (:h1 ,title)
-       ,@body)))
+    `(progn
+       (pushnew ',name *demos*)
+       (define-story ,(symb 'demo- name) (:title ,title :modules ,modules)
+        (:h1 ,title)
+        ,@body))))
 
 (define-demo trivial ())
 

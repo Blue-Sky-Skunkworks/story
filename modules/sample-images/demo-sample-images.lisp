8908eed0132775b82b881fdcf34f5ba74533967a
conrad <whalliburton@gmail.com>
2016-05-10 10:21:07 -0600

    !

--- modules/sample-images/demo-sample-images.lisp
+++ modules/sample-images/demo-sample-images.lisp
@@ -1,5 +1,5 @@
 (in-package :story)
 
 (define-demo sample-images (:sample-images)
-  (iter (for word in (split-sequence #\space "This is a set of test images for your reading pleasure."))
+  (iter (for word in (split-sequence #\space "This is a set of test images for your reading pleasure!"))
         (htm (:img :alt word :src (format nil "sample-images/~A.png" word)))))

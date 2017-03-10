f6734599cb3bee9ef3922189ca2af74109c15dd3
Student <student@alpha>
2017-03-10 03:49:12 -0700

    more fabric work

--- modules/fabric/demo-fabric.lisp
+++ modules/fabric/demo-fabric.lisp
@@ -1,6 +1,6 @@
 (in-package :story)
 
-(define-demo fabric ((:fabric))
+(define-demo fabric ((:fabric) :directories (("modules/demo-images" "samples")))
   (:button :onclick (ps (hitme)) "testme")
   (:canvas :width 200 :height 200 :id "c")
   (script
@@ -13,8 +13,13 @@
                               :height 20
                               :angle 45))))
     ((@ *c* add) *r*)
+    (defvar *i*)
+    ((@ fabric *image from-u-r-l) "samples/eye.png"
+     (lambda (img) (setf *i* img) ((@ *c* add) img)))
     (defun hitme ()
       ((@ *r* set) (create :left (* ((@ *math random)) 200)
+                           :top (* ((@ *math random)) 200)))
+      ((@ *i* set) (create :left (* ((@ *math random)) 200)
                            :top (* ((@ *math random)) 200)))
       ((@ *c* render-all)))))
 

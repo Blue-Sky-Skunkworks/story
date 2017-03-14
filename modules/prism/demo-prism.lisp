50926495370910599ea62394d0c46a38662b8f4b
Student <student@alpha>
2017-03-14 11:44:34 -0600

    prism syntax highlighter

---
+++ modules/prism/demo-prism.lisp
@@ -0,0 +1,5 @@
+(in-package :story)
+
+(define-demo prism ((:prism))
+  (:pre (:code :class "language-css" "p { color: red }"))
+  (:pre (:code :class "language-js" "random(1+2);")))
---
+++ modules/prism/prism.lisp
@@ -0,0 +1,5 @@
+(in-package :story)
+
+(define-story-module prism
+  :scripts ("prism.js")
+  :stylesheets ("themes/prism.css"))

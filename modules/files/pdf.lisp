e66a402fee8e07bf55bb4b5dec8eb0d9b6ef3b49
Student <student@alpha>
2017-03-17 21:08:08 -0600

    pdf thumbnails

---
+++ modules/files/pdf.lisp
@@ -0,0 +1,17 @@
+(in-package :story)
+
+(defun pdf-info (file)
+  (remove-if #L(member (cdr %) '("no" "none") :test #'equal)
+             (mapcar #L(split-on-first-occurance
+                        % #\: #L(string-trim '(#\space) %))
+                     (run/lines `("pdfinfo" ,file)))))
+
+(defmethod additional-file-information ((type (eql :application/pdf)) file)
+  (list (cons :info (pdf-info file))
+        (cons :thumbnail (create-image-thumbnail (extract-pdf-page file 1)))))
+
+(defun extract-pdf-page (file page &optional (destination (story-file "modules/files/tmp-pdf-pages/")))
+  (ensure-directories-exist destination)
+  (let ((name (f "~A~A-p~A" destination (pathname-name file) page)))
+    (run `(pdfimages "-png" "-f" ,page "-l" ,page ,file ,name))
+    (f "~A-000.png" name)))

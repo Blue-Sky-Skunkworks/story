eadb0f039dcbaf800f2f5937eac0b0c7dc523006
Student <student@alpha>
2017-03-18 13:54:03 -0600

    twemoji

--- modules/files/demo-files.lisp
+++ modules/files/demo-files.lisp
@@ -2,8 +2,7 @@
 
 (define-demo files ((:files) :directories (("modules/demo-files" "files")))
   (:div :id "files")
-  (script
-    (render-file-listing "files" "/files/")))
+  (script (render-file-listing "files" "/files/")))
 
 (defun create-demo-file-listing ()
   (save-file-listing (story-file "modules/demo-files/")))

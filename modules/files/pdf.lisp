a4cb2cd61e3754b8c3e42b710d38a25dad640418
Student <student@alpha>
2017-03-20 08:53:32 -0600

    use tesseract

--- modules/files/pdf.lisp
+++ modules/files/pdf.lisp
@@ -10,11 +10,13 @@
   (list (cons :info (pdf-info file))
         (cons :thumbnail (create-image-thumbnail (f "~A[0]" file)))))
 
-(defun extract-pdf-page (file page &optional (destination (story-file "modules/files/tmp-pdf-pages/")))
+(defun extract-pdf-page (file page &key ocr (destination (story-file "modules/files/tmp-pdf-pages/")))
   (ensure-directories-exist destination)
   (let ((name (f "~A~A-p~A" destination (pathname-name file) page)))
     (run `(pdfimages "-png" "-f" ,page "-l" ,page ,file ,name))
-    (f "~A-000.png" name)))
+    (let ((img-name (f "~A-000.png" name)))
+      (when ocr (run `(tesseract ,img-name ,(f "~A-ocr.txt" img-name))))
+      img-name)))
 
 (defun render-pdf-info (stream file)
   (html

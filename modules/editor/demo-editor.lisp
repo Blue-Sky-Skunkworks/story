380ca31ae6a8df251bf487740d16aad740cfb5b6
Student <student@alpha>
2017-03-27 05:23:42 -0600

    misc

--- modules/editor/demo-editor.lisp
+++ modules/editor/demo-editor.lisp
@@ -4,5 +4,4 @@
   (:div :id "editor" :style "width:600px;height:500px;"
         (fmt "~A~%~%~A~%" "Hello! Edit me!"
              (as-string (fractal 186 3 3 :char #\black_diamond))))
-  (script
-    (defvar *editor* ((@ ace edit) "editor"))))
+  (script ((@ ace edit) "editor")))

b1de5e6f499b23a6f2ffcc17026b21723aeeb926
Student <student@alpha>
2017-04-08 05:05:52 -0600

    define-template-method

--- modules/polymer/templates.lisp
+++ modules/polymer/templates.lisp
@@ -1,7 +1,59 @@
 (in-package :story)
 
+(defvar *templates* (make-hash-table))
+
 (defmacro define-template (name &key style properties content methods)
-  (let ((method-names (iter (for method in methods) (collect (car method)))))
+  `(progn
+     (setf (gethash ',name *templates*) (list ',style ',properties ',content ',methods
+                                              (html-to-string ,@content)))
+     (defun ,(symb name '-template) () (create-template ',name))))
+
+(defvar *template-methods* (make-hash-table))
+
+(defmacro define-template-method (name template args &body body)
+  (when-let (hit (gethash template *template-methods*))
+    (setf (gethash template *template-methods*)
+          (remove name hit :key #'car)))
+  (push (list name args body) (gethash template *template-methods*))
+  nil)
+
+(defun template-methods (name) (gethash name *template-methods*))
+
+(defun create-template (name)
+  (destructuring-bind (style properties content methods content-html) (gethash name *templates*)
+    (declare (ignore content))
+    (let ((methods (append methods (template-methods name)))
+          (method-names (iter (for method in methods) (collect (car method)))))
+      (let ((sname (format nil "~(~A~)" name))
+            (props (loop for (name type value) in properties
+                         appending
+                         `(,name (create type ,(string-capitalize type)
+                                         ,@(when value `(value ,(if (symbolp value)
+                                                                    (symbol-value value) value))))))))
+        (html-to-string
+          (:dom-module :id sname
+                       (:template
+                        (when style (htm (:style (str (cl-css:css style)))))
+                        (str content-html)))
+          (:script
+            (str
+             (ps*
+              `(*polymer
+                (create is ,sname properties (create ,@props)
+                        ,@(iter (for (name args . body) in methods)
+                            (appending
+                             (list name
+                                   `(lambda ,args
+                                      (macrolet ,(iter (for name in method-names)
+                                                   (collect `(,name (&rest args)
+                                                                    (cons (list '@ 'this ',name)
+                                                                          args))))
+                                        ,@body)))))))))))))))
+
+
+(defmacro define-template (name &key style properties content methods)
+  (let ((methods (append methods (template-methods name)))
+        (method-names (iter (for method in methods) (collect (car method)))))
     (let ((sname (format nil "~(~A~)" name))
           (props (loop for (name type value) in properties
                        appending

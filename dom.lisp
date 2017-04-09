cadd19a41ab2a0f7247a1e55031de58daa767853
Student <student@alpha>
2017-04-09 06:57:59 -0600

    resolve-multiple-props

--- dom.lisp
+++ dom.lisp
@@ -1,5 +1,14 @@
 (in-package :story-js)
 
+(defun resolve-multiple-props (properties)
+  (let (rtn)
+    (loop for (k v) in properties
+          do (if (consp k)
+                 (loop for x in k
+                       do (push (list x v) rtn))
+                 (push (list k v) rtn)))
+    (nreverse rtn)))
+
 (defpsmacro dom (args &rest children)
   (destructuring-bind (node-type &optional class-name properties inner-html) (ensure-list args)
     (let ((el (gensym))
@@ -8,7 +17,7 @@
          ,@(when class-name `((aand ,class-name (add-class ,el it))))
          ,@(when inner-html `((setf (getprop ,el 'inner-h-t-m-l) ,inner-html)))
          ,@(when properties
-             (loop for (k v) in properties
+             (loop for (k v) in (resolve-multiple-props properties)
                    collect
                       (if (story:starts-with-p (string-downcase k) "on-")
                           `((@ this listen) ,el ,(subseq (string-downcase k) 3) ,v)

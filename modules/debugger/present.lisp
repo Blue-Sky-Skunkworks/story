86a3c09952374fdd4cfba9118df23aebf69c3bd7
Student <student@alpha>
2017-04-10 07:43:01 -0600

    object-name-aliases

--- modules/debugger/present.lisp
+++ modules/debugger/present.lisp
@@ -7,6 +7,11 @@
      (setf *object-presentors* (remove ',type *object-presentors* :key #'car :test #'equal))
      (push (list ',type ',(if (second body) `(progn ,@body) (first body))) *object-presentors*)))
 
+(defvar *object-name-aliases* nil)
+
+(defmacro define-object-name-alias (from to)
+  `(pushnew (list ',from ',to) *object-name-aliases* :key #'car :test #'equal))
+
 (define-object-presentor "#text" (@ el text-content))
 (define-object-presentor "title" (@ el text-content))
 (define-object-presentor "h1" (@ el text-content))
@@ -31,6 +36,7 @@
 (define-object-presentor "BatteryManager"
   (mkstr (* (@ el level) 100) "%" (when (@ el charging) " charging")))
 
+(define-object-name-alias "CSSStyleDeclaration" "css")
 (define-object-presentor "CSSStyleDeclaration" (text (@ el css-text)))
 
 (define-template-method debugger-interface _present-obj (type el)
@@ -43,34 +49,36 @@
            (when (and (stringp id) (plusp (@ id length))) (dom (:span "id") id)))))
 
 (define-template-method debugger-interface present (element &optional fn-this)
-  (let ((type (type-of element)))
-    (cond
-      ((eql type "number") element)
-      ((eql type "boolean") (if element "true" "false"))
-      ((eql type "function") type
-       (dom (:span "desc" (((on-tap on-keypress) "_handlePresentEvent")
-                           (presenting element) (tab-index 1)
-                           (_fn-this fn-this)))
-            (mkstr "[" (@ element name) "]")))
-      ((eql type "object")
-       (if (eql element nil)
-           "null"
-           (let* ((type (or (try ((@ element node-name to-lower-case)) (:catch (error) nil))
-                            ((@ ((@ *object prototype to-string call) element) slice) 8 -1))))
-             (dom (:span (+ "desc" (if (eql type "Error") " error" ""))
-                         (((on-tap on-keypress) "_handlePresentEvent")
-                          (presenting element) (tab-index 1)))
-                  (text "<")
-                  (text (case type
-                          ("Error" (@ element name))
-                          (otherwise type)))
-                  ((@ this _present-obj call) this type element)
-                  (text ">")))))
-      ((eql type "string")
-       (dom (:span "prestring" (((on-tap on-keypress) "_handleTextExpansion")))
-            (+ "\"" element "\"")))
-      ((eql type "array") (+ "[" ((@ element to-string)) "]"))
-      ((eql type "undefined") type)
-      (t (+ "UNHANDLED: " type)))))
+  `(let ((type (type-of element))
+         (aliases (create ,@(iter (for (from to) in *object-name-aliases*)
+                              (appending (list from to))))))
+     (cond
+       ((eql type "number") element)
+       ((eql type "boolean") (if element "true" "false"))
+       ((eql type "function") type
+        (dom (:span "desc" (((on-tap on-keypress) "_handlePresentEvent")
+                            (presenting element) (tab-index 1)
+                            (_fn-this fn-this)))
+             (mkstr "[" (@ element name) "]")))
+       ((eql type "object")
+        (if (eql element nil)
+            "null"
+            (let* ((type (or (try ((@ element node-name to-lower-case)) (:catch (error) nil))
+                             ((@ ((@ *object prototype to-string call) element) slice) 8 -1))))
+              (dom (:span (+ "desc" (if (eql type "Error") " error" ""))
+                          (((on-tap on-keypress) "_handlePresentEvent")
+                           (presenting element) (tab-index 1)))
+                   (text "<")
+                   (text (case type
+                           ("Error" (@ element name))
+                           (otherwise (or (aref aliases type) type))))
+                   ((@ this _present-obj call) this type element)
+                   (text ">")))))
+       ((eql type "string")
+        (dom (:span "prestring" (((on-tap on-keypress) "_handleTextExpansion")))
+             (+ "\"" element "\"")))
+       ((eql type "array") (+ "[" ((@ element to-string)) "]"))
+       ((eql type "undefined") type)
+       (t (+ "UNHANDLED: " type)))))
 
 
